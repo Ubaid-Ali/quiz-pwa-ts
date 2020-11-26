a5979f27efbbcca38d651a8150116926549b9996
Ubaid-Ali <syedubaidali2020@gmail.com>
2020-11-27 04:48:19 +0500

    Add Api folder and Api.tsx file

---
+++ src/Api/Api.tsx
@@ -0,0 +1,31 @@
+import { shuffleArray } from '../Utilities/Utilities'
+
+
+export const fetchQuestions = async (amount: number, difficulty: Difficulty) => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const data = await (await fetch(endpoint)).json();
+
+    return data.results.map((question: Question) => (
+        {
+            ...question,
+            answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+        }
+    ))
+}
+
+
+export enum Difficulty {
+    EASY = 'easy',
+    MEDIUM = 'medium',
+    HARD = 'hard'
+};
+
+
+type Question = {
+    correct_answer: string,
+    difficulty: string,
+    incorrect_answers: string[]
+    question: string,
+    type: string
+}
+
