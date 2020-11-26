d9147460bcddb5912cc2051d16c8efff28bce270
Ubaid-Ali <syedubaidali2020@gmail.com>
2020-11-27 04:52:05 +0500

    add file QuestionCard.tsx

---
+++ src/Components/QuestionCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: any;
+    questionNum: number;
+    totalQuestions: number;
+}
+
+const QuestionCard = () => {
+    return <p>Questions</p>
+}
+
+export default QuestionCard;
