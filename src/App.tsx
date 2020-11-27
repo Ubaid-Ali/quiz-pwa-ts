f13e67b4a3a10a5d0af9ec162dbdd3ea42da3172
Ubaid-Ali <syedubaidali2020@gmail.com>
2020-11-27 05:06:54 +0500

    edit App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Difficulty, fetchQuestions } from './Api/Api';
 import './App.css';
 
+import QuestionCard from './Components/QuestionCard';
+
+
+const TOTAL_QUESTIONS = 10;
+
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
 function App() {
+
+
+  const [questions, setQuestions] = useState();
+  const [quizEnd, setQuizEnd] = useState<boolean>(true);
+  const [score, setScore] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [userAnswers, setUesrAnswers] = useState<AnswerObject[]>([]);
+  const [num, setNum] = useState(0);
+
+  const startQuiz = async () => {
+    fetchQuestions(10,Difficulty.EASY);
+    setQuizEnd(false);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+
+
+
+        {/* START BUTTON */}
+        <h1>Quiz App</h1>
+        {quizEnd || userAnswers.length === TOTAL_QUESTIONS ?
+          <button onClick={startQuiz}> Start Quiz </button> :
+        null}
+
+        {/* SCORE */}
+        {!quizEnd ? (<p>Score: {score} </p>) : null}
+
+        {/* LOADING */}
+        {loading ? <p>Loading...</p> : null}
+
+        {/* QUESTIONS ANSWERS*/}
+        {!loading && !quizEnd ? 
+          <QuestionCard
+
+          /> :
+        null}
+
+          {/* NEXT BUTTON */}
+          {!loading && !quizEnd && userAnswers.length === num+1 && num !== TOTAL_QUESTIONS -1 ?
+            <button>Next</button>
+          :null}
+
+      </div>
     </div>
   );
 }
